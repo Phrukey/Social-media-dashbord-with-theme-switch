694e99eac0b959a4742becedb6e5937ea7539d6c
Phrukey <muhdfaruuq@gmail.com>
2020-10-21 08:12:14 +0100

    Desktop View Completed

---
+++ themeSwitch.js
@@ -0,0 +1,6 @@
+const themeSwitch= document.querySelector("button");
+
+themeSwitch.addEventListener("click", (e) => {
+  themeSwitch.style.transform = "translateX(0px)"
+})
+console.log(themeSwitch)
