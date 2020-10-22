4b86c5de4b0e23e9310b89981ef95ae3d2f9234c
Phrukey <muhdfaruuq@gmail.com>
2020-10-22 12:23:02 +0100

    Added Some Icons to the markup

--- themeSwitch.js
+++ themeSwitch.js
@@ -1,6 +1,12 @@
 const themeSwitch= document.querySelector("button");
 
 themeSwitch.addEventListener("click", (e) => {
-  themeSwitch.style.transform = "translateX(0px)"
+  if(e.target.classList.contains("dark")) {
+    themeSwitch.style.transform = "translateX(0px)"
+    
+  } else {
+    themeSwitch.style.transform = "translateX(30px)"
+  }
+  
 })
 console.log(themeSwitch)
