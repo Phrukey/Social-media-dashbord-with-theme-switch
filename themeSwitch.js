1a59f86eccc32c99dd176316db41c59b898a3595
Phrukey <muhdfaruuq@gmail.com>
2020-10-22 15:09:03 +0100

    Styling correction

--- themeSwitch.js
+++ themeSwitch.js
@@ -2,10 +2,13 @@ const themeSwitch= document.querySelector("button");
 
 themeSwitch.addEventListener("click", (e) => {
   if(e.target.classList.contains("dark")) {
-    themeSwitch.style.transform = "translateX(0px)"
-    
+    themeSwitch.style.transform = "translateX(0px)";
+    themeSwitch.classList.remove("dark");
+
   } else {
-    themeSwitch.style.transform = "translateX(30px)"
+    themeSwitch.classList.add("dark");
+    themeSwitch.style.transform = "translateX(30px)";
+    e.target.parentElement.style.background = "linear gradient (to right, hsl(210, 78%, 56%), hsl(146, 68%, 55%))";
   }
   
 })
